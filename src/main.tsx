72e0c5fc9e65c7c98c69197a170315563ea07735
newcomer119 <mitarth.125035@stu.upes.ac.in>
2025-01-02 23:28:40 +0530

    made changes

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import './index.css';
 import './styles/animations.css';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import favicon from './faviconio.png'
+import favicon from './Audexis2.png'
 
 
 const rootElement = document.getElementById('root');
