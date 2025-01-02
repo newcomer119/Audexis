b08351996bb5fab68e862c93d78bc2a86f83dbb0
newcomer119 <mitarth.125035@stu.upes.ac.in>
2025-01-02 21:57:22 +0530

    added a favicon

--- src/main.tsx
+++ src/main.tsx
@@ -5,12 +5,19 @@ import './index.css';
 import './styles/animations.css';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import favicon from './faviconio.png'
 
 
 const rootElement = document.getElementById('root');
 if (!rootElement) throw new Error('Failed to find the root element');
 
 const root = createRoot(rootElement);
+
+// You can set it programmatically if needed
+const link = document.querySelector("link[rel~='icon']") as HTMLLinkElement;
+if (link) {
+    link.href = favicon;
+}
 
 root.render(
   <StrictMode>
